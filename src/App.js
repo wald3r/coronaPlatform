df607c7862955b197f2181132f10a9725180e482
Daniel Walder <walder.daniel@gmx.at>
2020-04-09 16:39:19 +0300

    update

--- src/App.js
+++ src/App.js
@@ -43,67 +43,74 @@ const App =() => {
   }, [])
 
     const globalStyle = {
-      backgroundColor: '#3d444a'       
-    }
-
-    const containerStyle = {
-      padding: '40px',
+      backgroundColor: '#202427', 
       textAlign: 'center',
-      position: 'absolute',
       color: '#FFFFFF',
-      transform: `translate(-50px, -50px)`,
-      backgroundColor: '#3d444a' 
-
+      
     }
     
     const table = {
       textAlign:'center',
       tableLayout: 'fixed',
-      width: '80%', 
+      width: '90%', 
+      marginLeft: '5%', 
+      marginRight: '5%',
+
+    }
+
+    const grid = {
+      display: 'grid',
+      gridTemplateColumns: "auto auto",
+      gridGap: '10px',
+      padding: '10px',
       marginLeft: '10%', 
       marginRight: '10%',
+    }
+
+    const singleGridSettings1 = {
+      backgroundColor: '#3d444a',
+      borderRadius: '25px',
+      alignItems: 'center',
+
+    }
+    const singleGridSettings2= {
+      backgroundColor: '#3d444a',
+      gridColumn: '1 / span 2',
+      borderRadius: '25px',
 
     }
 
     return (
       <div style={globalStyle}>
-          <div style={containerStyle}>
-            <br/>
-            <h2>COVID-19 Statistics</h2>
-            <br></br>
-            <Table responsive variant="dark" style={table} striped bordered hover>
+            <div style={grid}>
+              <div style={singleGridSettings1}>
+                <h2>COVID-19 Statistics</h2>
+              </div>
+              <div style={singleGridSettings1}>
+                <Numbers data={globalData}/>
+              </div>
+              <div style={singleGridSettings1}>
+                <GlobalBar  globalData={globalData}/>
+              </div>
+              <div style={singleGridSettings1}>
+                <Choropleth data={globalCountryConfirmed}/>
+              </div>
+              <div style={singleGridSettings1}>
+                <GlobalPie globalData={globalData}/>
+
+              </div>
+              <div style={singleGridSettings1}>
+              <Table responsive  style={table} >
               <tbody>
-              <tr>
-                <td colSpan={2} >
-                    <Numbers data={globalData}/>
-                  </td>
-              </tr>
-              <tr>
-                  <td colSpan={2} >
-                    <Choropleth 
-                      data={globalCountryConfirmed}
-                    />
-                  </td>
-                </tr>
-                <tr >
-                  <td style={{verticalAlign: 'middle'}}>
-                    Worldwide cases of the Coronavirus
-                    <GlobalBar  globalData={globalData}/>
-                  </td>
-                  <td >
-                    Worldwide distribution of confirmed cases
-                    <GlobalPie globalData={globalData}/>
-                  </td>
-                </tr>
+       
                 <tr>
-                  <td style={{verticalAlign: 'middle'}}> 
+                  <td style={{verticalAlign: 'middle', color:'#ffffff'}}> 
                     A bar chart, which shows all cases per country. <br /><br />
                     <Button size='sm' variant={countryChar === 'confirmed' ? 'secondary' : 'outline-secondary'} onClick={() => setCountryChar('confirmed')}>Confirmed</Button>
                     <Button size='sm' variant={countryChar === 'active' ? 'secondary' : 'outline-secondary'} onClick={() => setCountryChar('active')}>Active</Button>
                     <Button size='sm' variant={countryChar === 'recovered' ? 'secondary' : 'outline-secondary'} onClick={() => setCountryChar('recovered')}>Recovered</Button>
                     <Button size='sm' variant={countryChar === 'death' ? 'secondary' : 'outline-secondary'} onClick={() => setCountryChar('death')}>Death</Button><br />
-                  </td>
-                  <td >
+                 
                     {countryChar === 'confirmed' ? <Countries data={globalCountryConfirmed} info={'Confirmed'} color={globalColor.confirmed}/> : ''}
                     {countryChar === 'active' ? <Countries data={globalCountryActive} info={'Active'} color={globalColor.active}/> : ''}
                     {countryChar === 'recovered' ? <Countries data={globalCountryRecovered} info={'Recovered'} color={globalColor.recovered}/> : ''}
@@ -112,7 +119,8 @@ const App =() => {
                 </tr>
               </tbody>
             </Table>
-          </div>
+              </div>
+            </div>
           <br></br>
           <Footer />
       </div>
